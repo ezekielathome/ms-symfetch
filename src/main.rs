3661ae7958a2d98c4b4a5391e0bdf8542ea778f0
ezekiel <ezekiel@arcadian.garden>
2023-11-04 15:14:19 +0100

    main: small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fs::{self, File};
 use std::io;
 use std::path::PathBuf;
@@ -23,31 +24,31 @@ struct Options {
 
 const SYMBOL_ENDPOINT: &str = "https://msdl.microsoft.com/download/symbols";
 
-fn main() -> Result<(), ureq::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     let opts = Options::parse();
-    let mut id = String::new();
 
-    let part = format!("0000000{:X}", opts.timestamp);
-    id.push_str(&part[part.len() - 8..]);
-    id.push_str(&format!("{:x}", opts.virtual_size));
+    // construct file id from timestamp and virtual_size
+    let id = format!("{:08X}{:x}", opts.timestamp, opts.virtual_size);
 
+    // request the binary from the symbol endpoint
     let url = format!("{SYMBOL_ENDPOINT}/{0}/{1}/{0}", opts.name, id);
     let res = ureq::get(&url).call()?;
 
-    let name = match opts.output {
-        Some(mut buf) => {
-            // ensure path exists
-            let _ = fs::create_dir_all(&buf);
-
-            buf.push(opts.name);
-            buf.display().to_string()
+    // determine the output path
+    let name = match &opts.output {
+        Some(buf) => {
+            // create path if it doesn't exist
+            fs::create_dir_all(buf).ok();
+            buf.join(&opts.name)
         }
-        None => opts.name,
+        None => PathBuf::from(&opts.name),
     };
 
     // create output file
-    let mut output = File::create(name).expect("failed to create file!");
-    io::copy(&mut res.into_reader(), &mut output).expect("failed to copy stream to file!");
+    let mut output = File::create(name)?;
+
+    // copy response stream into output file
+    io::copy(&mut res.into_reader(), &mut output)?;
 
     Ok(())
 }
